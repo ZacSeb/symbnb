b21f78249ff97ea3dc11af8ff9292c9162e1277f
ZacSeb <86783847+ZacSeb@users.noreply.github.com>
2021-08-05 14:19:10 +0200

    installation et page d'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,8 +6,27 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HomeController extends AbstractController
-{
+class HomeController extends AbstractController{
+
+    /**
+     * @route("/bonjour/{prenom}/age/{age}", name="hello")
+     * @route("/salut", name="hello_base")
+     * @route("/bonjour/{prenom}", name="hello_prenom")
+     * 
+     * Montre la page qui dit bonjour
+     * 
+     * @return void
+     */
+    
+    public function hello($prenom = "anonyme", $age = 0){
+        return $this->render(
+            'hello.html.twig',
+            [
+                'prenom' => $prenom,
+                'age' => $age
+            ]
+        );
+    }
 
     /**
      * @Route("/", name="homepage")
